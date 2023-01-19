82da4fcaf8d5f4b9096e1967a6d9a11bde284415
rogofan <sacokov@gmail.com>
2023-01-19 23:28:12 +0100

    First commit

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+// import styled from "styled-components";
+import styles from "./Button.module.css";
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
